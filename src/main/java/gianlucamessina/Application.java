66843d9d11fde50d623c1c1635c6c853e08d9291
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-08-02 11:46:15 +0200

    creati metodi per ricerca documenti per ISBN, anno ed autore

--- src/main/java/gianlucamessina/Application.java
+++ src/main/java/gianlucamessina/Application.java
@@ -1,9 +1,13 @@
 package gianlucamessina;
 
 import gianlucamessina.entities.Documento;
+import gianlucamessina.entities.Libro;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Application {
     private List<Documento> archivio;
@@ -22,6 +26,18 @@ public class Application {
     }
 
     public void rimuoviElementoDatoIsbn(String isbn) {
-        archivio.removeIf(documento -> documento.getIsbn() == isbn);
+        archivio.removeIf(documento -> Objects.equals(documento.getIsbn(), isbn));
+    }
+
+    public Optional<Documento> ricercaPerIsbn(String isbn) {
+        return archivio.stream().filter(documento -> Objects.equals(documento.getIsbn(), isbn)).findFirst();
+    }
+
+    public List<Documento> ricercaPerAnnoPubblicazione(int anno) {
+        return archivio.stream().filter(documento -> documento.getAnnoPubblicazione() == anno).collect(Collectors.toList());
+    }
+
+    public List<Libro> ricercaPerAutore(String autore) {
+        return archivio.stream().filter(documento -> documento instanceof Libro && Objects.equals(((Libro) documento).getAutore(), autore)).map(documento -> (Libro) documento).collect(Collectors.toList());
     }
 }
